fab23c17861be03520134fa6122c35c805f3f509
LiZoMark841619 <githubs001@gmail.com>
2025-01-04 23:21:25 +0100

    ok

--- apples_oranges.py
+++ apples_oranges.py
@@ -1,8 +1,5 @@
-import array
-
-def countApplesAndOranges(s, t, a, apples_or_oranges):
-    validation_range, tree_loc = range(s, t+1), a
-    print(sum(1 for location in apples_or_oranges if tree_loc + location in validation_range))
+def countApplesAndOranges(s: int, t: int, a: int, apples_or_oranges: map):
+    print(sum(1 for location in apples_or_oranges if a + location in range(s, t+1)))
 
 if __name__ == '__main__':
     s, t = map(int, input().split())
@@ -10,7 +7,6 @@ if __name__ == '__main__':
     m, n = map(int, input().split())
     
     apples, oranges = map(int, input().split()), map(int, input().split())
-    apples, oranges = array.array('q', apples), array.array('q', oranges)
     
     countApplesAndOranges(s, t, a, apples)
     countApplesAndOranges(s, t, b, oranges)
