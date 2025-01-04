69c7df77a21933e373fe297088df6e1bd5bdad49
LiZoMark841619 <githubs001@gmail.com>
2025-01-05 00:08:25 +0100

    refactored_some_files

--- budget_2_items.py
+++ budget_2_items.py
@@ -1,31 +1,16 @@
 import sys
-from itertools import combinations
+from itertools import product
 
-def getMoneySpent(keyboards, drives, b):
-    keyboard_list = list(combinations(keyboards, 1))
-    drive_list = list(combinations(drives, 1))
-    
-    result = []
-    for val_1 in keyboard_list:
-        for val_2 in drive_list:
-            result.append(val_1+val_2)
-    
-    printing_result = []      
-    for value in result:
-        final = sum(list(value))
-        if final <= b:
-            printing_result.append(final)
-    printing_result.sort()
-    return printing_result[-1] if printing_result else -1
+def getMoneySpent(keyboards: list, drives: list, b: int):
+    result = list(product(keyboards, drives))
+    printing_result = [sum(list(value)) for value in result if sum(list(value)) <= b]      
+    return max(printing_result, default=-1)
             
 if __name__ == '__main__':
     fptr = sys.stdout
-    bnm = input().split()
-    b = int(bnm[0])
-    n = int(bnm[1])
-    m = int(bnm[2])
-    keyboards = list(map(int, input().rstrip().split()))
-    drives = list(map(int, input().rstrip().split()))
+    b, n, m = map(int, input().split())
+    keyboards = map(int, input().rstrip().split())
+    drives = map(int, input().rstrip().split())
     moneySpent = getMoneySpent(keyboards, drives, b)
     fptr.write(str(moneySpent) + '\n')
     fptr.close()
