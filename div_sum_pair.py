b99450806e147bd8c58ac79218fe44364b2e744b
LiZoMark841619 <githubs001@gmail.com>
2025-01-05 15:12:39 +0100

    ok

--- div_sum_pair.py
+++ div_sum_pair.py
@@ -1,17 +1,27 @@
 from itertools import combinations
 
-def divisibleSumPairs(k, ar):
-    combination_list = list(combinations(ar, 2))
-    final = [value for value in combination_list if (value[0] + value[1]) % k == 0]
-    return len(final)    
+def divisibleSumPairs(k: int, ar: list) -> len:
+    combination_list = combinations(ar, 2)
+    return len(list(filter(lambda x: (x[0]+x[1])% k == 0, combination_list)))  
         
 if __name__ == '__main__':
-    n, k = map(int, input().rstrip().split())
+    n = 0
+    k = 0
+    while n not in range(2, 101) or k not in range(1, 101):
+        inputs = input().split()
+        try:
+            n += int(inputs[0])
+            k += int(inputs[1])
+        except ValueError:
+            print('Only integer is allowed! Try again! ')
+        
     while True:
-        ar = list(map(int, input().rstrip().split()))
-        if len(ar) == n: 
-            result = divisibleSumPairs(k, ar)
-            print(result)
-            break
-        else:
-            print('Invalid list length! Try again! ')
+        try:
+            ar = list(map(int, input().split()))
+            if len(ar) == n and all(value in range(1, 101) for value in ar): 
+                print(divisibleSumPairs(k, ar))
+                break
+            else:
+                print('Invalid list length! Try again! ')
+        except ValueError:
+            print('Only integer is allowed! Try again! ')
