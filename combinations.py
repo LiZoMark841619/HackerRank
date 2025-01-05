5a59cf7b2c3f3b6df070f0b6f3924fe4e63ac166
LiZoMark841619 <githubs001@gmail.com>
2025-01-05 09:42:48 +0100

    refactored

--- combinations.py
+++ combinations.py
@@ -2,17 +2,14 @@ from itertools import combinations
 
 if __name__ == '__main__':
     while True:
+        S, k = input().upper().split()
         try:
-            S, k = input('Enter a string and an integer to see its combinations until that number! ').strip().upper().split(' ')
-            string_list = list(S)
             k = int(k)
-            if 0 < k <= len(S):
-                counter = 1
-                while counter <= k:
-                    combinations_ = list(combinations(sorted(string_list), counter))
+            if k in range(1, len(S)+1):
+                for counter in range(1, k+1):
+                    combinations_ = combinations(sorted(S), counter)
                     for value in combinations_:
                         print(''.join(value))
-                    counter += 1
                 break
             print(f'Enter a valid k size from 1 to {len(S)}')
         except ValueError:
--- combinations_with_replacement.py
+++ combinations_with_replacement.py
@@ -1,14 +1,15 @@
 from itertools import combinations_with_replacement
 
-while True:
-    S, k = input().upper().split()
-    try:
-        k = int(k)
-        if k in range(1, len(S)+1):
-            combinations_ = combinations_with_replacement(sorted(list(S)), k)
-            for value in combinations_:
-                print(''.join(value))
-            break
-        print(f'Enter a valid k size from 1 to {len(S)}')
-    except ValueError:
-        print('Only integer is allowed! Try again! ')
+if __name__ == '__main__':
+    while True:
+        S, k = input().upper().split()
+        try:
+            k = int(k)
+            if k in range(1, len(S)+1):
+                combinations_ = combinations_with_replacement(sorted(list(S)), k)
+                for value in combinations_:
+                    print(''.join(value))
+                break
+            print(f'Enter a valid k size from 1 to {len(S)}')
+        except ValueError:
+            print('Only integer is allowed! Try again! ')
