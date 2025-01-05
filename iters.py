90e5d5f118bf9873a36c0ee30fbf25d65fff1f12
LiZoMark841619 <githubs001@gmail.com>
2025-01-05 22:07:46 +0100

    ok

--- iters.py
+++ iters.py
@@ -1,8 +1,6 @@
 from string import ascii_lowercase
 from itertools import combinations
 
-char_options = ascii_lowercase
-
 N = 0
 while N not in range(1, 11):
     N += int(input())
@@ -10,7 +8,7 @@ while N not in range(1, 11):
 string_list = []
 while len(string_list) != N:
     for value in input().split():
-        if value not in char_options:
+        if value not in ascii_lowercase:
             continue
         string_list.append(value)
 
@@ -18,9 +16,8 @@ K = 0
 while K not in range(1, N+1):
     K += int(input())
 
-idxs = list(range(1, len(string_list) +1))
+idxs = range(1, len(string_list) + 1)
 result = list(combinations(idxs, K))
-# print(result)
 counter = 0
 for value in result:
     for val in value:
