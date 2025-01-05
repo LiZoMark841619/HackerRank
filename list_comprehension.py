c28e6db83ea3786d9130374b58044541d9c6aacd
LiZoMark841619 <githubs001@gmail.com>
2025-01-05 22:27:55 +0100

    refactored_list_comprehension_with_product

--- list_comprehension.py
+++ list_comprehension.py
@@ -6,4 +6,12 @@ n = int(input())
 result_new = [[x_value, y_value, z_value] for z_value in range(z+1) for y_value in range(y+1) for x_value in range(x+1)]
 result_new.sort()
 final = [value for value in result_new if sum(value) != n]
+print(result_new)
 print(final)
+
+from itertools import product
+
+alter_result_new = list(product(range(x+1), range(y+1), range(z+1)))
+alter_final = list(filter(lambda x: sum(list(x)) != n, alter_result_new))
+print(alter_result_new)
+print(alter_final)
