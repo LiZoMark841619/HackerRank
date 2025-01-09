80ba76765ef41f4fdc1cac3638eafa01fa18cafa
LiZoMark841619 <githubs001@gmail.com>
2025-01-09 22:48:53 +0100

    final_budget_2_items.py

--- budget_2_items.py
+++ budget_2_items.py
@@ -4,12 +4,16 @@ from itertools import product
 def make_a_list(lenght: int, min_val: int, max_val: int) -> list:
         some_list = []
         while True:
-            values = input().split()
+            values = input(f'Setup a list! Length must be {lenght}! Values between: {min_val}-{max_val}: ').split()
             try:
                 vals = list(map(int, values))
-                if len(vals) == lenght and all(value in range(min_val, max_val+1) for value in vals):
+                if len(vals) != lenght:
+                    print('Invalid length! Try again! ')
+                elif all(value in range(min_val, max_val+1) for value in vals):
                     some_list.extend(vals)
                     return some_list
+                else:
+                    print('You value(s) is/are out of range! ')
             except ValueError:
                 print('Only integer is allowed! ')
                 
@@ -19,15 +23,20 @@ def getMoneySpent(keyboards: list, drives: list, b: int)-> int:
 if __name__ == '__main__':
     fptr = sys.stdout
     while True:
+        values = input('Setup budget, and the number of keyboards and drives: ').split()
         try:
-            b, n, m = map(int, input().split())
-            if b in range(1, 10**6+1) and (n and m) in range(1, 1001):
+            bnm = list(map(int, values))
+            if len(bnm) != 3:
+                print('Your list is invalid! It must have 3 values! ')
+            elif bnm[0] not in range(1, 10**6+1) or bnm[1] not in range(1, 1001) or bnm[2] not in range(1, 1001):
+                print('Your values are out of range! Try again! ')
+            else:
                 break
         except ValueError:
             print('Only integer is allowed! Try again!')
     
-    keyboards = make_a_list(n, 1, 10**6)
-    drives = make_a_list(m, 1, 10**6)
-    moneySpent = getMoneySpent(keyboards, drives, b)
+    keyboards = make_a_list(bnm[1], 1, 10**6)
+    drives = make_a_list(bnm[2], 1, 10**6)
+    moneySpent = getMoneySpent(keyboards, drives, bnm[0])
     fptr.write(str(moneySpent) + '\n')
     fptr.close()
