e21a033c1cb243f1eaab481db1812d5481ee78ed
LiZoMark841619 <githubs001@gmail.com>
2025-01-09 22:14:12 +0100

    replace_list_comprehension_with_generator

--- budget_2_items.py
+++ budget_2_items.py
@@ -1,16 +1,33 @@
 import sys
 from itertools import product
 
-def getMoneySpent(keyboards: list, drives: list, b: int):
-    result = list(product(keyboards, drives))
-    printing_result = [sum(list(value)) for value in result if sum(list(value)) <= b]      
-    return max(printing_result, default=-1)
+def make_a_list(lenght: int, min_val: int, max_val: int) -> list:
+        some_list = []
+        while True:
+            values = input().split()
+            try:
+                vals = list(map(int, values))
+                if len(vals) == lenght and all(value in range(min_val, max_val+1) for value in vals):
+                    some_list.extend(vals)
+                    return some_list
+            except ValueError:
+                print('Only integer is allowed! ')
+                
+def getMoneySpent(keyboards: list, drives: list, b: int)-> int:
+    return max((s for s in (sum(pair) for pair in product(keyboards, drives)) if s <= b), default=-1)
             
 if __name__ == '__main__':
     fptr = sys.stdout
-    b, n, m = map(int, input().split())
-    keyboards = map(int, input().rstrip().split())
-    drives = map(int, input().rstrip().split())
+    while True:
+        try:
+            b, n, m = map(int, input().split())
+            if b in range(1, 10**6+1) and (n and m) in range(1, 1001):
+                break
+        except ValueError:
+            print('Only integer is allowed! Try again!')
+    
+    keyboards = make_a_list(n, 1, 10**6)
+    drives = make_a_list(m, 1, 10**6)
     moneySpent = getMoneySpent(keyboards, drives, b)
     fptr.write(str(moneySpent) + '\n')
     fptr.close()
