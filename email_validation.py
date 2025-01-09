d58605165683783bfcae9cd538a8acee72cc7cf9
Balazs Arva <balazsarva1982@gmail.com>
2025-01-09 23:28:24 +0100

    Update email_validation.py

--- email_validation.py
+++ email_validation.py
@@ -4,7 +4,7 @@ username = ascii_lowercase+ascii_uppercase+digits+'_-'
 website = username[:-2]
 extension = website[:-10]
 
-def filtering(valid_chars: str, unexamined_string: str) -> str:
+def validate(valid_chars: str, unexamined_string: str) -> str:
     return ''.join(list(filter(lambda x: x in valid_chars, unexamined_string)))
 
 def fun(s: str) -> bool:
@@ -14,7 +14,7 @@ def fun(s: str) -> bool:
     if '.' not in rest[-4:]:
         return False
     web, ext = rest.split('.')
-    return filtering(username, user) == user and filtering(website, web) == web and filtering(extension, ext) == ext and len(ext) <= 3
+    return validate(username, user) == user and validate(website, web) == web and validate(extension, ext) == ext and len(ext) <= 3
     
 def filter_mail(emails):
     return list(filter(fun, emails))
