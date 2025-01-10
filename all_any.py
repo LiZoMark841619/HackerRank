0ee4ca5bd1283e1eec0941c257e107081505532c
LiZoMark841619 <githubs001@gmail.com>
2025-01-10 23:35:17 +0100

    ok

--- all_any.py
+++ all_any.py
@@ -1,30 +1,30 @@
 if __name__ == '__main__':
-    N = 0
     while True:
         value = input('Set a value between 1 and 1000! ')
         try:
             value = int(value)
             if value in range(1, 1001):
-                N += value
                 break
             else:
-                print('Out of range! ')
+                print('Out of range! Please enter a number between 1 and 1000! ')
         except ValueError:
             print('Only integer is allowed! Try again! ')
             
     some_list: list[int] = []
     while True:
         values = input(f'Create a list! Its length must be {value}! ').split()
+        if len(values) != value:
+            print(f'Invalid number of elements! It must contain {value} values! Try again! ')
+            continue
         try:
             int_values = list(map(int, values))
-            if len(int_values) == N:
-                some_list.extend(int_values)
-                break
+            some_list = int_values
+            break
         except ValueError:
-            print('Only integer is allowed! Try again! ')
+            print('All elements must be integers! Try again! ')
 
     first_conditional = all(num >= 0 for num in some_list)
     values = [list(str(value)) for value in some_list]
-    reversed_values = [list(reversed(value)) for value in values]
+    reversed_values = [list(reversed(str_value)) for str_value in values]
     second_conditional = any(values[i] == reversed_values[i] for i in range(len(values)))
     print(all([first_conditional, second_conditional]))
