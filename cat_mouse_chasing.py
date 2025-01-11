819813236939257284fd047112402c0fe61d92de
LiZoMark841619 <githubs001@gmail.com>
2025-01-11 22:21:57 +0100

    ok

--- cat_mouse_chasing.py
+++ cat_mouse_chasing.py
@@ -1,13 +1,10 @@
-import sys
-
 def catAndMouse(x, y, z):
     return 'Cat B' if abs(z - y) < abs(z - x) else 'Mouse C' if abs(z - y) == abs(z - x) else 'Cat A'
 
 if __name__ == '__main__':
-    fptr = sys.stdout
-    q = int(input())
+    q = int(input('Enter the number of queries: '))
 
     for _ in range(q):
-        x, y, z = map(int, input().split())
+        x, y, z = map(int, input('Enter the position of Cat A, Cat B and the Mouse: ').split())
         result = catAndMouse(x, y, z)
-        fptr.write(str(result) + '\n')
+        print(result)
