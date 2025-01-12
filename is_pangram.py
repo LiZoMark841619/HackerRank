b403b2c16014065b91cc3e415d2e7a83a2194a4b
LiZoMark841619 <githubs001@gmail.com>
2025-01-12 14:38:12 +0100

    refactored_codes

--- is_pangram.py
+++ is_pangram.py
@@ -1,5 +1,25 @@
 from string import ascii_lowercase as chars
 
-def is_pangram(string_: str) -> bool:
+def is_pangram_one(string_: str) -> bool:
     return ''.join(sorted(set(filter(lambda x: x in chars, string_.lower())))) == chars
-print(is_pangram('The quick brown fox jumps over the lazy dog.'))
+
+def is_pangram_two(string_: str) -> bool:
+    return ''.join(sorted({c for c in string_ if c in chars})) == chars
+
+def is_pangram_three(string_: str) -> bool:
+    return set(chars).issubset(set(string_.lower()))
+
+def is_pangram_four(st: str) -> bool:  # sourcery skip: invert-any-all, use-any, use-next
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    for char in alphabet:
+        if not st.lower().count(char):
+            return False
+    return True
+
+test = 'The quick brown fox jumps over the lazy dog.'
+print(is_pangram_one(test)) # True
+print(is_pangram_two(test)) # True
+print(is_pangram_three(test)) # True
+print(is_pangram_four(test)) # True
+
+
