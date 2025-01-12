6cd0c4e7875f1f27600e699f80406f84ba2d7221
LiZoMark841619 <githubs001@gmail.com>
2025-01-12 22:00:58 +0100

    final version of the counting_birds.py

--- counting_birds.py
+++ counting_birds.py
@@ -1,5 +1,4 @@
 from collections import Counter
-import sys
 
 def migratoryBirds(arr):
     count_types_of_birds = Counter(arr)
@@ -7,13 +6,11 @@ def migratoryBirds(arr):
     return min(key for key in count_types_of_birds if count_types_of_birds[key] == max_value)
 
 if __name__ == '__main__':
-    fptr = sys.stdout
-    arr_count = int(input().strip())
+    arr_count = int(input("Enter the number of bird sightings: ").strip())
     while True:
-        arr = list(map(int, input().rstrip().split()))
+        arr = list(map(int, input("Enter the bird types: ").rstrip().split()))
         if len(arr) == arr_count:
             result = migratoryBirds(arr)
-            fptr.write(str(result) + '\n')
-            fptr.close()
+            print(result)
             break
-        print('You have more birds than free space! Try again! ')
+        print("The number of bird types entered does not match the expected count. Try again!")
