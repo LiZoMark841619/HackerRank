ef774a799e45ee8832bbcdce6adf13534ced2b51
LiZoMark841619 <githubs001@gmail.com>
2025-01-13 08:26:06 +0100

    final_piling_up.py

--- piling_up.py
+++ piling_up.py
@@ -1,25 +1,26 @@
 from collections import deque
 
-def get_valid_number(min_value: int, max_value: int) -> int:
+def get_valid_number(prompt: str, min_value: int, max_value: int) -> int:
     while True:
         try:
-            value = int(input())
+            value = int(input(prompt))
             if value in range(min_value, max_value+1):
                 return value
+            else:
+                print(f'Invalid range! Value must be between {min_value}-{max_value} Try again! ')
         except ValueError:
-            print('Invalid value! Try again with iteger! ')
+            print('Invalid value! Try again with integer! ')
 
 def make_a_deque(expected_length: int = 1) -> deque:
-    some_deque:deque[int] = deque()
     while True:
         try:
             values = list(map(int, input().split()))
             if len(values) == expected_length:
-                some_deque.extend(values)
-                break
+                return deque(values)
+            else:
+                print(f'Invalid number of values! It must contain {expected_length} values! Try again! ')
         except ValueError:
             print('Invalid input. Please enter integers only.')
-    return some_deque
 
 def pop_left_right(some_deque: deque, empty: deque) -> deque:
     while some_deque:
@@ -27,9 +28,9 @@ def pop_left_right(some_deque: deque, empty: deque) -> deque:
     return empty
     
 if __name__ == '__main__':
-    T = get_valid_number(1, 5)
+    T = get_valid_number('Enter a valid iteration number from 1 to 5: ', 1, 5)
     for _ in range(T):
-        number_of_cubes = get_valid_number(1, 10**5)
+        number_of_cubes = get_valid_number('Enter the number of cubes: ', 1, 10**5)
         some_deque = make_a_deque(number_of_cubes)
         empty:deque[int] = deque()
         max_value, min_value = max(some_deque), min(some_deque)
