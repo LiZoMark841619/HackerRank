7d21f0a89372ef643d578750aeb82bd7583ee8f8
LiZoMark841619 <githubs001@gmail.com>
2025-01-13 16:58:45 +0100

    refactored div_sum_pair.py

--- div_sum_pair.py
+++ div_sum_pair.py
@@ -2,26 +2,20 @@ from itertools import combinations
 
 def divisibleSumPairs(k: int, ar: list) -> int:
     combination_list = combinations(ar, 2)
-    return len(list(filter(lambda x: (x[0]+x[1])% k == 0, combination_list)))  
+    return len(list(filter(lambda x: (x[0] + x[1]) % k == 0, combination_list)))  
         
-if __name__ == '__main__':
-    n = 0
-    k = 0
-    while n not in range(2, 101) or k not in range(1, 101):
-        inputs = input().split()
-        try:
-            n += int(inputs[0])
-            k += int(inputs[1])
-        except ValueError:
-            print('Only integer is allowed! Try again! ')
-        
+def get_valid_input(prompt: str, condition) -> int | list:
     while True:
         try:
-            ar = list(map(int, input().split()))
-            if len(ar) == n and all(value in range(1, 101) for value in ar): 
-                print(divisibleSumPairs(k, ar))
-                break
+            values = list(map(int, input(prompt).split()))
+            if condition(values):
+                return values
             else:
-                print('Invalid list length! Try again! ')
+                print("Invalid input. Try again!")
         except ValueError:
             print('Only integer is allowed! Try again! ')
+
+if __name__ == '__main__':
+    n, k = get_valid_input('Enter n and k: ', lambda x: len(x) == 2 and x[0] in range(2, 101) and x[1] in range(1, 101))
+    valid_list = get_valid_input('Enter n integers: ', lambda x: len(x) == n and all(value in range(1, 101) for value in x))
+    print(divisibleSumPairs(k, valid_list))
