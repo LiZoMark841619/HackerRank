69b4932a73ae49a1855f21279e5d11955fc539e6
LiZoMark841619 <githubs001@gmail.com>
2025-01-13 21:00:07 +0100

    final_float_validating.py

--- float_validating.py
+++ float_validating.py
@@ -1,25 +1,32 @@
+import re
+
 def valid_float_number(string: str) -> bool:
     
     if string.count('.') != 1 or string[-1] == '.':
         return False
-    else:
-        if string[0] == '.':
-            return True if string[1:].isdigit() else False
+    
+    elif string[0] == '.':
+        return bool(string[1:].isdigit())
 
-        elif string.startswith('+') or string.startswith('-'):
-            if string[1] == '.':
-                return True if string[2:].isdigit() else False
-            else:
-                return True if string[1:string.find('.')].isdigit() and string[string.find('.')+1:].isdigit() else False
-
-        elif string[0].isdigit():
-            return True if string[:string.find('.')].isdigit() and string[string.find('.')+1:].isdigit() else False
+    elif string.startswith('+') or string.startswith('-'):
+        if string[1] == '.':
+            return bool(string[2:].isdigit())
         else:
-            return False
-        
+            return bool(string[1:string.find('.')].isdigit() and string[string.find('.')+1:].isdigit())
+
+    elif string[0].isdigit():
+        return bool(string[:string.find('.')].isdigit() and string[string.find('.')+1:].isdigit())
+    else:
+        return False
+
+def valid_float_number_alter(string: str) -> bool:
+    pattern = r'^[+-]?\d*\.\d+$'
+    return bool(re.match(pattern, string))
+
 if __name__ == '__main__':
-    T = 0
-    while T not in range(1, 10):
-        T += int(input())
+    while True:
+        T = int(input())
+        if T in range(1, 10):
+            break
     for _ in range(T):
-        print(valid_float_number(input()))
+        print(valid_float_number_alter(input()))
