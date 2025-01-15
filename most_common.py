6257ad4b6ce1820fd35d7641deac0152d77687e4
LiZoMark841619 <githubs001@gmail.com>
2025-01-15 21:59:10 +0100

    finalized most_common.py

--- most_common.py
+++ most_common.py
@@ -1,14 +1,15 @@
 from collections import Counter
             
-def get_valid_string() -> str:
-    s = ''
-    while len(s) not in range(3, 10**4) and len(set(s)) <= 3:
-        s += input()
-    return s
+def get_valid_string(prompt, condition) -> str:
+    while True:
+        s = input(prompt)
+        if condition(s):
+            return s
+        else:
+            print("Invalid input. Try again!")
 
 if __name__ == '__main__':
-    s = list(get_valid_string())
-    s.sort()
+    s = sorted(get_valid_string('', lambda x: len(x) in range(3, 10**4+1) and len(set(x)) >= 3))
     most_common = Counter(s).most_common()[:3]
     for val in most_common:
         print(*val)
