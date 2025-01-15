e0bc9cd892e68a5831cd54bb70a050cc8ea85be7
LiZoMark841619 <githubs001@gmail.com>
2025-01-16 00:09:24 +0100

    before tested, but refactored

--- namedtuple.py
+++ namedtuple.py
@@ -1,13 +1,34 @@
 from collections import namedtuple
 
-N = int(input())
-column_names = input().split()
+while True:
+    try:
+        N = int(input('Enter the number of students: '))
+        if N in range(1, 5):
+            print(f'You have entered {N} students! ')
+            break
+        else:
+            print('The number of students must be between 1 and 5! Try again! ')
+    except ValueError:
+        print('Invalid input! Try again! ')
+    
+column_names = input('Enter the column names: ').split()
 Student = namedtuple('Student', column_names)
 
 marks = []
 for _ in range(N):
-    stu = Student(*input().split())
-    marks.append(stu.MARKS)
+    while True:
+        values = input(f'Enter the data of the student #{_}: ').split()
+        try:
+            stu = Student(*values)
+        except AttributeError:
+            print('AttributeError! Try again with valid attributes! ')
+        except TypeError:
+            print('TypeError! Try again with valid inputs! ')
+        except ValueError:
+            print('ValueError! Try again with valid inputs! ')
+        else:
+            marks.append(stu.MARKS)
+            break
 
 total = 0
 for value in marks:
